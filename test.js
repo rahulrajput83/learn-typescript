821ad8252633ef258c67826f9cc2b0f32f38eeef
Rahul Rajput <rahulrajput21112@gmail.com>
2023-06-23 15:04:21 +0530

    Update

--- test.js
+++ test.js
@@ -1,2 +1,15 @@
-var id;
-id = 5;
+"use strict";
+// Basic Types
+let id;
+id = 5; // Correct
+// id = "One" // Error: Type 'string' is not assignable to type 'number'.
+let company;
+company = 'Unknown'; // Correct
+// company = 50; // Error: Type 'number' is not assignable to type 'string'.
+let isCompleted;
+isCompleted = true; // Correct
+// isCompleted = "falseTrue" // Error: Type 'string' is not assignable to type 'boolean'.
+let x;
+x = 'Hello'; // Correct
+x = 10; // Correct
+x = true; // Correct
--- test.ts
+++ test.ts
@@ -1,3 +1,17 @@
+// Basic Types
 let id: number;
+id = 5; // Correct
+id = "One" // Error: Type 'string' is not assignable to type 'number'.
 
-id = 5;
+let company: string;
+company = 'Unknown'; // Correct
+company = 50; // Error: Type 'number' is not assignable to type 'string'.
+
+let isCompleted: boolean;
+isCompleted = true; // Correct
+isCompleted = "falseTrue" // Error: Type 'string' is not assignable to type 'boolean'.
+
+let x: any;
+x = 'Hello' // Correct
+x = 10 // Correct
+x = true // Correct
