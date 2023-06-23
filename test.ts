c8c63195cff352fa42a4efa19671ed731eb3dfec
Rahul Rajput <rahulrajput21112@gmail.com>
2023-06-23 15:14:42 +0530

    Update

--- test.ts
+++ test.ts
@@ -15,3 +15,11 @@ let x: any;
 x = 'Hello' // Correct
 x = 10 // Correct
 x = true // Correct
+
+let ids: number[];
+ids = [1, 2, 4, 5, 6] // Correct
+ids = ['John', 1, 2] // Error: Type 'string' is not assignable to type 'number'.
+ids = [1, 2, 5, true] // Error: Type 'boolean' is not assignable to type 'number'.
+
+let Ids: any[];
+Ids = [1, 2, 3, true, 'String'] // Correct
